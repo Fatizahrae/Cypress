5c5be2eb406daa2cc1a99c6797499362bec65717
Fatizahrae <abbadi@weringgroup.com>
2022-10-02 10:58:09 +0200

    firstComit

---
+++ cypress/integration/examples/fistTest.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+    it('should visit ',() => {
+      const newLocal = 'https://www.google.com'
+      cy.visit(newLocal)
+      cy.get('#L2AGLb > .QS5gu').click()
+    })
+    
+    it('should visit ',() => {
+      const newLocal = 'https://www.google.com'
+      cy.visit(newLocal)
+      cy.get('#L2AGLb > .QS5gu').click()
+    })
